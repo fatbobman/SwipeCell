86ca50ffd8a03732ecdf716a5e1d3a6918c6735c
Yang Xu <xuyang@me.com>
2020-10-30 13:41:52 +0800

    删除了swipecell .开始查找mac版的适配问题

--- Package.swift
+++ Package.swift
@@ -5,7 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "SwipeCell",
-    platforms: [.iOS(.v14)],
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v10_13),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
